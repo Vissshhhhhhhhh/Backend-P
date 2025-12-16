e000f8fccd4b2249785b564ab31d5c163a01f12c
Vissshhhhhhhhh <viswanathpaarthiban1@gmail.com>
2025-12-16 21:48:12 +0530

    File Stream/Buffer

--- fs.js
+++ fs.js
@@ -7,18 +7,23 @@ if (!fs.existsSync("./data"))
   });
 
 if (fs.existsSync("./data")) {
-  fs.readFile("./data/file.txt", (err, data) => { // readFile, writeFile - > async func
+  fs.writeFile("./data/file.txt",'I am here' ,(err) => { // readFile, writeFile - > async func
     if (err) {
       console.log(err.message);
-    } else console.log(data.toString());
+    }
   });
 }
 
-if (fs.existsSync("./data/file.txt")) {
-  fs.unlink("./data/file.txt", (e) => {          // fs.unlink - > Synchronous function
-    if (e) console.log(e.message);
-    else console.log("File deleted");
-  });
-}
+// if (fs.existsSync("./data/file.txt")) {
+//   fs.unlink("./data/file.txt", (e) => {          // fs.unlink - > Synchronous function
+//     if (e) console.log(e.message);
+//     else console.log("File deleted");
+//   });
+// }
 
-// STREAMS
+// Streams
+
+const readstream = fs.createReadStream('./data/file.txt',{encoding : 'utf8'});
+readstream.on('data',(buffer)=>{
+    console.log(buffer);
+})
